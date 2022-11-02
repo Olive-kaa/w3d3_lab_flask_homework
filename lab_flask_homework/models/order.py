f23fead52d3c8ed30981637a22e9f307a22156d0
Olive-kaa <olive.kaa@gmail.com>
2022-11-02 21:47:39 +0000

    homework submission

--- lab_flask_homework/models/order.py
+++ lab_flask_homework/models/order.py
@@ -3,12 +3,12 @@ from datetime import datetime
 class Order():
 
     def __init__(self, customer_name, quantity, pizza_name, extra_toppings):
-        now = datetime.now()
+        date = datetime.now()
         
         self.customer_name = customer_name
         self.quantity = quantity
         self.pizza_name = pizza_name
-        self.order_date = now.strftime("%H: %M: %S")
+        self.order_date = date.strftime("%H: %M: %S")
         self.extra_toppings = extra_toppings
         
         
